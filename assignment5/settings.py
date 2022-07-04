fa1ffb4d41a3240de0be178b4be76f852eca101e
Rrezart Saliu <saliurrezart@gmail.com>
2022-07-04 23:29:25 +0200

    final1

--- assignment5/settings.py
+++ assignment5/settings.py
@@ -135,6 +135,7 @@ django_heroku.settings(locals())
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
